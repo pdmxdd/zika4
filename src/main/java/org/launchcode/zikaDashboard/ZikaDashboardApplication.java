51b8168cabaf12a2ab4656ecb716953c7f5ac1c9
paul <pdmxdd@gmail.com>
2019-04-09 12:09:25 -0500

    Initial Commit
    Project from Spring Initilzer

---
+++ src/main/java/org/launchcode/zikaDashboard/ZikaDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.zikaDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikaDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZikaDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/zikaDashboard/ZikaDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.zikaDashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZikaDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
